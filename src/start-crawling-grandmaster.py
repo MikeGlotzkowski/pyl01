b022f9f959814a1b9c667fe11ba599b54448aaec
sl <sebastian.lischewski@outlook.com>
2020-05-02 17:42:29 +0200

    fix(crawler): now crawle grandmaster and master again

--- src/start-crawling-grandmaster.py
+++ src/start-crawling-grandmaster.py
@@ -4,11 +4,11 @@ import database as db
 
 connection = db.get_connection()
 collection = db.get_connection_to_collection(
-    connection, riot_api.Available_tiers.challenger.name)
+    connection, riot_api.Available_tiers.grandmaster.name)
 number_of_matches_per_player = 200
 
 riot_api.init_logger()
-mr = MatchRunner(collection, riot_api.Available_tiers.challenger.name,
+mr = MatchRunner(collection, riot_api.Available_tiers.grandmaster.name,
                  number_of_matches_per_player)
 mr.init_logger()
 mr.run()
